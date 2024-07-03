0029ac73c1ddecdd3874524cf6f54af1932b7409
Lucas Bordini <lucas.bordini@hotmail.com>
2024-07-03 16:59:43 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,13 +22,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VisaSensoryBranding",
-            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v6.0/VisaSensoryBranding.xcframework.zip",
+            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v7.0/VisaSensoryBranding.xcframework.zip",
             checksum: "03ff1cbb5d50dc3d68521506628549d15c26744ecfbf5565d647716bb6118c80",
         )
         .binaryTarget(
             name: "ReadyRemitSDK",
-            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v6.0/ReadyRemitSDK.zip",
-            checksum: "1fc5657c2708d9a1fce717fbd5ddb79a6a396aa02f65f04be51af0fe91727c02"
+            url: "https://github.com/BrightwellPayments/readyremit-sdk-ios/releases/download/v7.0/ReadyRemitSDK.zip",
+            checksum: "1b77c0b171e15d768afbe42a338d922b7f7b913f901e0b6da2c203f2873f93ca"
         ),
     ],
     swiftLanguageVersions: [.v5]
